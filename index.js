0f2b74f798a2cbc764e628951a180287cf39b4df
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 14:28:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+const printBadges = (array) => {
+  for(let i = 0; i < array.length; i++){
+    console.log(`Welcome ${array[i]}! You are employee #${i+1}.`);
+  } 
+  return array;
+}
+
+const tailsNeverFails = () => {
+  let tails = 0;
+  while(Math.random() >= 0.5){
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
