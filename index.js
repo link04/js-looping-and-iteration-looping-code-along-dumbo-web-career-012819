06d11ddf42f500d61d2d074c3553526b53cd9fab
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 14:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const printBadges = (array) => {
 const tailsNeverFails = () => {
   let tails = 0;
   while(Math.random() >= 0.5){
-    tails++
+    tails++;
   }
-  console.log(`You got ${tails} tails in a row!`)
+  console.log(`You got ${tails} tails in a row!`);
 }
