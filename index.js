6bc5850030a226f154238bd373de7f7f07ed5731
Maximo Bautista <max.bautista97@gmail.com>
2019-03-15 14:16:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const printBadges = (array) => {
   for(let i = 0; i < array.length; i++){
-    console.log(Welcome Joe! You are employee #1.array[i]);
+    console.log(`Welcome Joe! You are employee #1.`array[i]);
   } 
 }
